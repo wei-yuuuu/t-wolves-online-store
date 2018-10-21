f55d763aeb1720f2e991c674f2d5da5fbf9e08e1
takeu <tony40508@gmail.com>
2018-10-22 01:50:36 +0800

    ✨ add components

---
+++ frontend/components/Header.js
@@ -0,0 +1,16 @@
+import Nav from './Nav'
+
+const Header = () => (
+  <div>
+    <div className="bar">
+      <a href="">Minny T-Wolves</a>
+      <Nav />
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+)
+
+export default Header
