f211282387001e470fe47815507b2a50a69044fb
takeu <tony40508@gmail.com>
2019-03-19 00:50:11 +0800

    ✨ upload images by cloudinary

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -33,11 +33,35 @@ class CreateItem extends Component {
     largeImage: '',
     price: 0,
   }
+
   handleChange = e => {
     const { name, type, value } = e.target
     const val = type === 'number' ? parseFloat(value) : value
     this.setState({ [name]: val })
   }
+
+  uploadFile = async e => {
+    console.log('uploading file...')
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    data.append('upload_preset', 't-wolves')
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/dnf64lzut/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      },
+    )
+    const file = await res.json()
+    console.log(file)
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    })
+  }
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -45,8 +69,8 @@ class CreateItem extends Component {
           <Form
             onSubmit={async e => {
               e.preventDefault()
+
               const res = await createItem()
-              console.log(res)
 
               Router.push({
                 pathname: '/item',
@@ -66,6 +90,25 @@ class CreateItem extends Component {
                   required
                   value={this.state.title}
                   onChange={this.handleChange}
+                />
+                {this.state.image && (
+                  <img
+                    width="200"
+                    src={this.state.image}
+                    alt="Upload Preview"
+                  />
+                )}
+              </label>
+
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
                 />
               </label>
 
@@ -93,6 +136,7 @@ class CreateItem extends Component {
                   onChange={this.handleChange}
                 />
               </label>
+
               <button type="submit">Submit</button>
             </fieldset>
           </Form>
