b4129051cdaa23de1adae42c319eed6d4a87064f
takeu <tony40508@gmail.com>
2019-03-18 15:43:13 +0800

    🚧 wrapping apollo provider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,32 @@
 import App, { Container } from 'next/app'
+import { ApolloProvider } from 'react-apollo'
 import Page from '../components/Page'
+import withData from '../lib/withData'
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    pageProps.query = ctx.query
+    return { pageProps }
+  }
   render() {
-    const { Component } = this.props
+    const { Component, apollo, pageProps } = this.props
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     )
   }
 }
 
-export default MyApp
+export default withData(MyApp)
