94209d9f76c5694c646883f486e6074df94ddf91
takeu <tony40508@gmail.com>
2019-03-21 14:44:28 +0800

    ✨ add pagination query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding')
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
 }
 
 module.exports = Query
