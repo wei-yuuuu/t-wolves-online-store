1cc6f14f51319724abda359cb272a71b5477b12e
takeu <tony40508@gmail.com>
2019-03-27 00:11:39 +0800

    🚧 feat(signup) backend signup flow (jwt token as cookie, bcrypt password)

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem(
@@ -30,6 +33,29 @@ const Mutations = {
     const item = await ctx.db.query.item({ where }, `{ id title}`)
 
     return ctx.db.mutation.deleteItem({ where }, info)
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase()
+
+    const password = await bcrypt.hash(args.password, 10)
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info,
+    )
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    // set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    })
+
+    return user
   },
 }
 
