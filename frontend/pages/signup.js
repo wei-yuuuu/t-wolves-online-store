8b0930b8108d2688e56cd3c7f0a3c3725c06fd38
takeu <tony40508@gmail.com>
2019-03-29 23:52:52 +0800

    🚧 wip: signup mutation comp

---
+++ frontend/pages/signup.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import Signup from '../components/Signup'
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`
+
+const SignupPage = props => (
+  <Columns>
+    <Signup />
+    <Signup />
+    <Signup />
+  </Columns>
+)
+
+export default SignupPage
