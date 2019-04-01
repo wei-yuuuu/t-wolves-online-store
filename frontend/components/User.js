bdba8950136db1229d3a6e6ddd1f6330e35ef03e
takeu <tony40508@gmail.com>
2019-04-01 23:42:41 +0800

    ✨ feat: User render props comp

---
+++ frontend/components/User.js
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag'
+import PropTypes from 'prop-types'
+import { Query } from 'react-apollo'
+
+const CURRENT_USER_QUERY = gql`
+  query {
+    me {
+      id
+      email
+      name
+      permissions
+    }
+  }
+`
+
+const User = props => (
+  <Query {...props} query={CURRENT_USER_QUERY}>
+    {payload => props.children(payload)}
+  </Query>
+)
+
+User.PropTypes = {
+  children: PropTypes.func.isRequired,
+}
+
+export default User
+export { CURRENT_USER_QUERY }
