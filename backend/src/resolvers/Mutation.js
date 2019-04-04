a7f3f050ececf46bbb5de9c2ead74197f4e4dea1
takeu <tony40508@gmail.com>
2019-04-04 23:58:50 +0800

    ✨ feat: add signout mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -78,6 +78,10 @@ const Mutations = {
     // 5. Return the user
     return user
   },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token')
+    return { message: 'Sign Out!' }
+  },
 }
 
 module.exports = Mutations
