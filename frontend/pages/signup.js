659f3fde416884a2d5c4ad75d072e4257fd325ad
takeu <tony40508@gmail.com>
2019-04-22 01:58:42 +0800

    ✨ feat: reset password page

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import Signin from '../components/Signin'
 import Signup from '../components/Signup'
+import RequestReset from '../components/RequestReset'
 
 const Columns = styled.div`
   display: grid;
@@ -12,6 +13,7 @@ const SignupPage = props => (
   <Columns>
     <Signup />
     <Signin />
+    <RequestReset />
   </Columns>
 )
 
