e09fd1851f77ca9573a918b33a7f7c149e27fb83
takeu <tony40508@gmail.com>
2019-05-15 00:13:23 +0800

    feat: wrapped with gated sign in comp

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,9 +1,12 @@
 import CreateItem from '../components/CreateItem'
+import PleaseSignIn from '../components/PleaseSignIn'
 
 const Sell = props => (
-  <div>
-    <CreateItem />
-  </div>
+  <>
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
+  </>
 )
 
 export default Sell
