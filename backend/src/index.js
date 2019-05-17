5a66e07ae54d2e8271e69bbe53a1d8aaa8b586da
takeu <tony40508@gmail.com>
2019-05-17 19:57:53 +0800

    wip: permissions

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,6 +20,18 @@ server.express.use((req, res, next) => {
   next()
 })
 
+// Create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next()
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    '{ id, permissions, email, name }',
+  )
+  req.user = user
+  next()
+})
+
 server.start(
   {
     cors: {
