da2b4020ac5f85c1cd8ed653f3e26b18982835c2
takeu <tony40508@gmail.com>
2019-05-21 01:57:37 +0800

    chore: catch error on clicking deleteItem

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -32,7 +32,9 @@ class DeleteItem extends Component {
           <button
             onClick={() => {
               if (confirm('Are you sure you want to delete this item?')) {
-                deleteItem()
+                deleteItem().catch(err => {
+                  alert(err.message)
+                })
               }
             }}
           >
