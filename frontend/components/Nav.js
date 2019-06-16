d832f7c52f90428547f002641e83dece2cc6a667
takeu <tony40508@gmail.com>
2019-06-17 00:16:53 +0800

    chore: add Cart Comp to exact position

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import Signout from './Signout'
 import NavStyles from './styles/NavStyles'
 import User from './User'
+import { Mutation } from 'react-apollo'
+import { TOGGLE_CART_MUTATION } from './Cart'
 
 const Nav = () => (
   <User>
@@ -22,6 +24,9 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+            </Mutation>
           </>
         )}
         {!me && (
