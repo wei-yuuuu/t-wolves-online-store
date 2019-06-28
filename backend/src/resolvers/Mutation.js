447b1bfd8309e9ca2cc19c6e13b3d855b8b90cb7
takeu <tony40508@gmail.com>
2019-06-28 23:33:23 +0800

    feat: server side add to cart

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -202,6 +202,44 @@ const Mutations = {
       info,
     )
   },
+  async addToCart(parent, args, ctx, info) {
+    const { userId } = ctx.request
+    if (!userId) {
+      throw new Error('You must be signed in soooon')
+    }
+    // Query the users current cart
+    // this return a promise, so it needs await
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id },
+      },
+    })
+    // Check if that item is already in their cart and increment by 1 if it is
+    if (existingCartItem) {
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 },
+        },
+        info,
+      )
+    }
+    // If its not, create a fresh CartItem for that user!
+    return ctx.db.mutation.createCartItem(
+      {
+        data: {
+          user: {
+            connect: { id: userId },
+          },
+          item: {
+            connect: { id: args.id },
+          },
+        },
+      },
+      info,
+    )
+  },
 }
 
 module.exports = Mutations
