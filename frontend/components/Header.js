feeb9094a0440235b7b0f2fa5b9537c29948c96b
takeu <tony40508@gmail.com>
2019-07-08 20:54:32 +0800

    chore: add search comp

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 import Router from 'next/router'
-import NProgress from 'nprogress'
 import styled from 'styled-components'
+import NProgress from 'nprogress'
+
 import Nav from './Nav'
 import Cart from './Cart'
+import Search from './Search'
 
 Router.onRouteChangeStart = () => {
   NProgress.start()
@@ -66,7 +68,7 @@ const Header = () => (
       <Nav />
     </div>
     <div className="sub-bar">
-      <p>Search</p>
+      <Search />
     </div>
     <Cart />
   </StyledHeader>
