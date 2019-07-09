ffbd10488c70b5aafdd2768c188897e2cba8bde8
takeu <tony40508@gmail.com>
2019-07-10 01:10:58 +0800

    chore: wrap TakeMyMoney Comp

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import { Query, Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
+import React from 'react'
 import { adopt } from 'react-adopt'
-
-import User from './User'
-import CartItem from './CartItem'
-
-import CartStyles from './styles/CartStyles'
-import Supreme from './styles/Supreme'
-import CloseButton from './styles/CloseButton'
-import SickButton from './styles/SickButton'
-
+import { Mutation, Query } from 'react-apollo'
 import calcTotalPrice from '../lib/calcTotalPrice'
 import formatMoney from '../lib/formatMoney'
+import CartItem from './CartItem'
+import CartStyles from './styles/CartStyles'
+import CloseButton from './styles/CloseButton'
+import SickButton from './styles/SickButton'
+import Supreme from './styles/Supreme'
+import TakeMyMoney from './TakeMyMoney'
+import User from './User'
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -59,7 +57,9 @@ const Cart = () => (
           </ul>
           <footer>
             <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-            <SickButton>Checkout</SickButton>
+            <TakeMyMoney>
+              <SickButton>Checkout</SickButton>
+            </TakeMyMoney>
           </footer>
         </CartStyles>
       )
