66650eb873c7ecba664f5b651a626cac71f20a0e
takeu <tony40508@gmail.com>
2019-07-20 18:44:23 +0800

    feat: query single orders

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -28,6 +28,29 @@ const Query = {
     // 3. if they do, query all the users!
     return ctx.db.query.users({}, info)
   },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You arent logged in!')
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id },
+      },
+      info,
+    )
+    // 3. Check if the have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      'ADMIN',
+    )
+    if (!ownsOrder && !hasPermissionToSeeOrder) {
+      throw new Error('You cant see this buddd')
+    }
+    // 4. Return the order
+    return order
+  },
 }
 
 module.exports = Query
