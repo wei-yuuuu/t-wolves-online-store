3ebe0a2a741c7ef0c010293867016b5abbbece2c
takeu <tony40508@gmail.com>
2019-07-27 02:08:07 +0800

    chore: line

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,6 @@ server.express.use((req, res, next) => {
 server.express.use(async (req, res, next) => {
   // if they aren't logged in, skip this
   if (!req.userId) return next()
-
   const user = await db.query.user(
     { where: { id: req.userId } },
     '{ id, permissions, email, name }',
