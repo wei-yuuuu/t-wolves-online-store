42f9abd082c3c8ccda80d285adb1687da2b519ae
takeu <tony40508@gmail.com>
2019-07-27 00:37:03 +0800

    fix: permissions href

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -24,7 +24,7 @@ const Nav = () => (
             <Link href="/orders">
               <a>Orders</a>
             </Link>
-            <Link href="/me">
+            <Link href="/permissions">
               <a>Account</a>
             </Link>
             <Signout />
